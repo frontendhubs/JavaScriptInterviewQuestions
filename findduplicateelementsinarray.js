0f0515fd7053633cb5bc0e9a228926c43adbffd9
frontendhubs <121153168+frontendhubs@users.noreply.github.com>
2023-02-17 10:42:32 +0530

    findduplicateelementsinarray

---
+++ findduplicateelementsinarray.js
@@ -0,0 +1,45 @@
+
+    let array =  [1, 2, 1, 3, 4, 3, 5];
+
+    //let duplicate = []
+
+    // output  1, 3
+
+    //  filter() method or for loop
+
+    // indexOf() method
+
+
+    // imperative way of writing  code
+
+    //  for(var i =0; i < array.length; i++)
+    //  {
+    //      //console.log(array[i])
+
+    //      if(array.indexOf(array[i]) != i)
+                                           
+    //      {
+    //              console.log(array[i])
+    //              duplicate.push(array[i])
+    //      }
+
+
+    //  }
+
+
+ // Declarative way of writing  code
+
+
+       let duplicate = array.filter((item, index) => {
+
+              return array.indexOf(item) != index
+
+       })
+
+         
+
+
+
+
+
+     console.log("duplicate", duplicate)
